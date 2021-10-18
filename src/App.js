e943fcf710bc368b31ff517cdf1ebee1b982f8f6
rysiek1147 <daw-kryst@wp.pl>
2021-10-18 20:08:57 +0200

    add Modul4 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Komponenty from "./pages/Komponenty-2";
 import Modul3 from "./pages/Modul3";
+import Modul4 from "./pages/Modul4";
 
 function App() {
   return (
@@ -19,11 +20,15 @@ function App() {
             <li>
               <Link to="/modul3">Moduł 3</Link>
             </li>
+            <li>
+              <Link to="/modul4">Moduł 4</Link>
+            </li>
           </ul>
         </header>
         <Switch>
           <Route path="/komponenty" component={Komponenty}/>
           <Route path="/modul3" component={Modul3}/>
+          <Route path="/modul4" component={Modul4}/>
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/Modul4.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, createRef } from "react";
+import BillFunc from "components/BillForm/BillFunc";
+import BillClass from "components/BillForm/BillClass";
+import { RefButton } from "components/Form/Button";
+import FormValid from "components/FormValid/FormValid";
+import FormValidHook from "components/FormValid/FormValidHook";
+import Budget from "components/Budget";
+import { BudgetContext } from "contexts";
+import { categories as data } from "components/Budget/data.json";
+
+function Modul4() {
+  const [budget, setBudget] = useState(() => {
+    const initValue = JSON.parse(localStorage.getItem("budget"));
+    return initValue || [];
+  });
+
+  const addToBudget = (data) => {
+    setBudget([...budget, data]);
+  }
+
+  const removeFromBudget = (id) => {
+    setBudget([...budget.filter(item => item.id !== id)]);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("budget",JSON.stringify(budget));
+  },[budget]);
+
+  const ref = createRef();
+  const handleMouseOver = () => {
+    const currentButton = ref.current;
+    currentButton.style.background="#f00";
+    currentButton.style.color="#fff";
+  }
+
+  return (
+    <div style={{width: "600px", margin: "0 auto"}}>
+      <h3>Tip component (function)</h3>
+      <BillFunc/>
+      <h3>Tip component (class)</h3>
+      <BillClass/>
+      <h3>Form Valid</h3>
+      <FormValid/>
+      <h3>Form Valid (RHF)</h3>
+      <FormValidHook/>
+      <h3>Budget</h3>
+      <BudgetContext.Provider value={{categories: data}}>
+        <Budget.Form addAction={addToBudget}/>
+        <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
+          <div style={{width: "50%"}}>
+            <Budget.List 
+              title="CashIn"
+              data={budget.filter((item)=> item.type === 0)} 
+              showSum
+              removeAction={removeFromBudget}
+            />
+          </div>
+          <div style={{width: "50%"}}>
+            <Budget.List
+              title="CashOut"
+              data={budget.filter((item)=> item.type === 1)} 
+              showSum
+              removeAction={removeFromBudget}
+            />
+          </div> 
+          <div style={{width: "100%"}}>
+          <Budget.List
+            title="Cash In&Out (in one list)"
+            data={budget}
+            removeAction={removeFromBudget}
+          />
+          </div>       
+        </div>
+        <div className="total">
+          <Budget.Sum data={budget} field="sum"/>
+        </div>
+      </BudgetContext.Provider>
+      <RefButton ref={ref} label="Label from prop" bgColor="belize hole" handleMouseOver={handleMouseOver}/>
+    </div>
+  )
+}
+
+export default Modul4;
