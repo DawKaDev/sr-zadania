19bc66e086a3d81f36b919b8a2e1f6f2d9822a21
rysiek1147 <daw-kryst@wp.pl>
2021-10-18 20:06:52 +0200

    rebuild uuid v4 gen

--- src/components/UUIDGen/index.js
+++ src/components/UUIDGen/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 function Uuid() {
-  const uid = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const char = Math.random() * 16 | 0;
-    const variant = Math.floor(Math.random() * 4 + 8); // available 8,9,a,b
-    let v = c === 'x' ? char : variant;
-    return v.toString(16);
-  })}
     return (
       <>
       {uid()}
       </>
     )
 }
+
+export function uid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  const char = Math.random() * 16 | 0;
+  const variant = Math.floor(Math.random() * 4 + 8); // available 8,9,a,b
+  let v = c === 'x' ? char : variant;
+  return v.toString(16);
+})}
 
 export default Uuid;
