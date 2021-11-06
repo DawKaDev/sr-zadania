641ed84d8a4b2975a2fe4d3e90d592086ecf2d3c
rysiek1147 <daw-kryst@wp.pl>
2021-11-06 17:13:05 +0100

    add placeholder to UserList

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
+
+import ReactPlaceholder from 'react-placeholder';
+import 'react-placeholder/lib/reactPlaceholder.css';
+
 import User from './User';
 
 export default function UserList() {
   const [users, setUsers] = useState([]);
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
+    let timer = null;
     const fetchData = async () => {
       await fetch("https://randomuser.me/api/?results=10")
       .then(result => result.json())
       .then(data => setUsers(data.results))
     }
     fetchData();
+    timer = setTimeout(()=>{
+      setIsReady(true);
+    }, 2000)
+    return () => clearTimeout(timer)
   },[]);
 
   return (
     <>
     <div className="users__box">
       {users.map(user => (
-        <User key={`u-${user.login.uuid}`} user={user}/>
+        <ReactPlaceholder key={`u-${user.login.uuid}`} type="media" ready={isReady} showLoadingAnimation rows={6}>
+          <User user={user}/>
+        </ReactPlaceholder>
       ))}
     </div>
     </>
