73f316d4581250d5b125a218f5e0bc1e6fc24a47
rysiek1147 <daw-kryst@wp.pl>
2021-12-11 19:46:17 +0100

    create UserDetails component

--- src/components/User/UserDetails.js
+++ src/components/User/UserDetails.js
@@ -1,10 +1,16 @@
 import { useLocation } from 'react-router-dom';
 import User from './User';
+import Map from './Map';
 
 export default function UserDetails() {
   const location = useLocation();
   const user = location.state.user;
   return (
-    <User user={user}/>
+    <div className="user__profile">
+      <User user={user}/>
+      <div className="user__map">
+        <Map coordinates={[user.location.coordinates.latitude, user.location.coordinates.longitude]}/>
+      </div>
+    </div>
   )
 }
