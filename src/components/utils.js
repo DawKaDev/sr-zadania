a261379ed78e780f21c1b196f645964e9e189ddd
rysiek1147 <daw-kryst@wp.pl>
2021-12-11 20:01:52 +0100

    add utils functions

--- src/components/utils.js
+++ src/components/utils.js
@@ -3,3 +3,16 @@ export function cutText(text, strLength = 25, ellipsis = "...") {
   ? text.substr(0,strLength-1) + ellipsis
   : text
 }
+
+export function formatDate(date, locales = "en-EN", options = { weekday: 'long', day: 'numeric', month: 'long' }){
+  const newDate = new Date(date);
+  return new Intl.DateTimeFormat(locales, options).format(newDate);
+}
+
+export function prepareMessage(content, keys, data) {
+  let newMessage = "";
+  keys.forEach((key, index) => {
+    newMessage = content.replaceAll(`{{ ${key} }}`, data[index])
+  })
+  return newMessage;
+}
