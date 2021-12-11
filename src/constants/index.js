184a674e1df920e04fe1bcc28aad6c6ed3e2e098
rysiek1147 <daw-kryst@wp.pl>
2021-12-11 20:00:24 +0100

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+export const INIT_PASSWORD = "123456";
+
+export const ACTION_TYPE = {
+  ADD: "add",
+  EDIT: "edit",
+  REMOVE: "remove",
+  SEND: "send"
+}
+
+export const CONTEXT = {
+  SUBSCRIBER: "subscriber",
+  CAMPAIGN: "campaign"
+}
