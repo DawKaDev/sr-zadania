6ccd67c1ad1ee024575e62aa21d679f495966348
rysiek1147 <daw-kryst@wp.pl>
2021-12-15 20:47:34 +0100

    update set new counter value error

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export default function Counter({start}) {
   const [counter, setCounter] = useState(start);
-  const [newCounter, setNewCounter] = useState();
+  const [newCounter, setNewCounter] = useState(0);
 
   const handleIncreaseClick = () => {
     setCounter(counter + 1);
@@ -24,7 +24,7 @@ export default function Counter({start}) {
       <button type='button' className='button counter__decrease' onClick={handleDecreaseClick}>-</button>
       <span className='counter__result'>{counter}</span>
       <button type='button' className='button counter__increase' onClick={handleIncreaseClick}>+</button>
-      <input type="number" name='counter' className='counter__input' onChange={handleSetStartChange}/>
+      <input type="number" name='counter' value={newCounter} className='counter__input' onChange={handleSetStartChange}/>
       <button type='button' className='button counter__set' onClick={handleSetCounter}>Set</button>
       <button type='button' className='button counter__reset' onClick={handleResetCounter}>Reset</button>
     </div>
