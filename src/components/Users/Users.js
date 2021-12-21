11f4afbb3f68c3287654d52ac39494c2cca1de64
rysiek1147 <daw-kryst@wp.pl>
2021-12-21 21:04:04 +0100

    add mock service worker

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 export default function Users(props) {
   const { users, fetchUsers, add } = props;
+
+  const mockHandler = async () => {
+    await fetch('/users',{
+      headers: {
+        'Content-Type': 'application.json'
+      }
+    }).then(response => response.json())
+    .then(data => console.log(data))
+  }
   return (
     <>
       {users.length && users.map((user, index) => (
         <p key={index}>{user.name}</p>
       ))}
-      <button onClick={()=> fetchUsers()}>Fetch</button>
+      <button onClick={() => fetchUsers()}>Fetch</button>
       <button onClick={() => add("Mark")}>Add new</button>
+      <button onClick={() => mockHandler()}>Mock</button>
     </>
   )
 }
--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -4,7 +4,6 @@ import "../actions/users";
 export default function usersReducer(state = [], action) {
   switch (action.type) {
     case FETCH_USERS:
-      const users = state.users;
       return [...state, {name: "Dave"}];
     case ADD_USERS:
       return [...state, {name: action.payload}];
