c6a1a326cf079d26fe3e737de5602f0721248a52
rysiek1147 <daw-kryst@wp.pl>
2022-01-05 13:41:00 +0100

    add Message to app

--- src/pages/Modul11.js
+++ src/pages/Modul11.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import UsersContainer from 'containers/UsersContainer';
+import MessageContainer from 'containers/MessagesContainer';
 
 export default function Modul11() {
   return (
-    <UsersContainer/>
+    <>
+      <MessageContainer/>
+      <UsersContainer/>
+    </>
   )
 }
