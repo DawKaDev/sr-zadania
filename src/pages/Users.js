e013395296b4508f85e245113279125810921b48
rysiek1147 <daw-kryst@wp.pl>
2022-01-08 16:16:53 +0100

    update Users page view

--- src/pages/Users.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { UsersContainer } from "components/UsersContainer/UsersContainer";
-
-export default function Users(){
-  return (
-    <UsersContainer/>
-  )
-}
---
+++ src/pages/UsersModule.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { UsersContainer } from "components/UsersContainer/UsersContainer";
+import Users from "containers/UsersContainer";
+
+export default function UsersModule(){
+  return (
+    <>
+      <UsersContainer/>
+      <Users/>
+    </>
+  )
+}
